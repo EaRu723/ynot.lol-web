55817484948d9bc9040eb7c183e34c5121ed011f
Arnav Surve <arnav@surve.dev>
2025-01-20 22:51:19 -0800

    update user model and create new session model
    users table is self explanatory and sessions table is to store active
    sessions for the application

--- ynot-server/app/models/models.py
+++ ynot-server/app/models/models.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timezone
 
-from sqlalchemy import (JSON, Boolean, Column, DateTime, ForeignKey, Integer,
-                        MetaData, String, Table, Text, func)
+from sqlalchemy import (JSON, Boolean, CheckConstraint, Column, DateTime,
+                        ForeignKey, Integer, MetaData, String, Table, Text,
+                        func)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -28,7 +29,7 @@ class Post(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     note = Column(Text, nullable=False)
-    urls = Column(JSON, nullable=True)
+    urls = Column(JSON, nullable=True)  # Parsed urls from post note
     file_keys = Column(JSON, nullable=True)  # S3 keys for uploaded files
     created_at = Column(DateTime, server_default=func.now(), nullable=False)
 
@@ -63,15 +64,43 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    google_id = Column(String, unique=True, index=True)
+    login_id = Column(String, unique=True, index=True)
+    ownid_data = Column(Text)
     email = Column(String, unique=True, index=True)
     name = Column(String)
     username = Column(String, unique=True)
+    bio = Column(Text, unique=True)
     avatar = Column(String, nullable=True)
     banner = Column(String, nullable=True)
     is_profile_complete = Column(Boolean, default=False)
+    created_at = Column(DateTime, server_default=func.now(), nullable=False)  # Etc/UTC
 
+    sessions = relationship(
+        "UserSession", back_populates="user", cascade="all, delete-orphan"
+    )
     posts = relationship("Post", back_populates="owner")
 
+    # __table_args__ = (
+    #     CheckConstraint("LENGTH(ownid_data) >= 5000", name="ownid_data_min_length"),
+    # )
+
     def __repr__(self):
-        return f"<User(id={self.id}, google_id={self.google_id}, email={self.email})>"
+        return f"<User(id={self.id}, google_id={self.login_id}, email={self.email})>"
+
+
+class UserSession(Base):
+    __tablename__ = "sessions"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    session_token = Column(String(64), unique=True, nullable=False, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    created_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    expires_at = Column(DateTime(timezone=True), nullable=False)
+    ip_address = Column(String, nullable=True)  # Optional: Store the user's IP address
+    user_agent = Column(
+        String, nullable=True
+    )  # Optional: Store the user's user agent string
+
+    user = relationship("User", back_populates="sessions")
