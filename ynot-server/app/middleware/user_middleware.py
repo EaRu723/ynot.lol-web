69a35ab97520ccb527a5da55ac9cc7665f87dbce
Arnav Surve <arnav@surve.dev>
2025-01-20 22:50:37 -0800

    fix async context manager usage and implement returning user object along with session

--- ynot-server/app/middleware/user_middleware.py
+++ ynot-server/app/middleware/user_middleware.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timezone
+
 from fastapi import HTTPException, Request
 from sqlalchemy import select
 from sqlalchemy.orm import joinedload
@@ -5,37 +7,53 @@ from starlette.middleware.base import (BaseHTTPMiddleware,
                                        RequestResponseEndpoint)
 from starlette.responses import Response
 
-from app.db.db import async_session
-from app.models.models import User
+from app.db.db import get_async_session
+from app.models.models import UserSession
 
 
 class LoadUserMiddleware(BaseHTTPMiddleware):
     async def dispatch(
-        self, request: Request, call_next: RequestResponseEndpoint
+        self,
+        request: Request,
+        call_next: RequestResponseEndpoint,
     ) -> Response:
-        user_id = request.session.get("user_id")
+        session_token = request.session.get("session_token")
 
-        async with async_session() as db:
-            if user_id:
-                query = select(User).where(User.id == user_id)
-                result = await db.execute(query)
-                user = result.scalar()
+        if session_token:
+            try:
+                async for db in get_async_session():
+                    query = (
+                        select(UserSession)
+                        .options(joinedload(UserSession.user))
+                        .where(UserSession.session_token == session_token)
+                    )
+                    result = await db.execute(query)
+                    session = result.scalar()
 
-                # Store the user in request.state for later use
-                if user:
-                    request.state.user = user
-                else:
-                    # If user_id is invalid, clear the session to avoid errors
-                    request.session.clear()
-                    request.state.user = None
-            else:
-                # If no user_id in session, mark the user as not logged in
+                    # Store the user in request.state for later use
+                    if session and session.expires_at > datetime.now(timezone.utc):
+                        request.state.session = session
+                        request.state.user = session.user
+                    else:
+                        # If session_id is invalid, clear the session to avoid errors
+                        request.session.clear()
+                        request.state.session = None
+                        request.state.user = None
+            except Exception as e:
+                print(f"Error loading session: {e}")
+                request.state.session = None
                 request.state.user = None
+                raise HTTPException(status_code=500, detail="Error loading session")
+
+        else:
+            # If no session_id in session, mark the user as not logged in
+            request.state.session = None
+            request.state.user = None
 
         return await call_next(request)
 
 
-async def login_required(request: Request):
-    if not request.state.user:
+async def login_required(request: Request) -> UserSession:
+    if not request.state.session:
         raise HTTPException(status_code=401, detail="Not authenticated")
-    return request.state.user
+    return request.state.session
