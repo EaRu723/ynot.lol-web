70f991e7d5c8c3dd304433fef89c164829a7012a
Arnav Surve <arnav@surve.dev>
2025-02-25 16:56:57 -0800

    ignore type warnings because SQLAlchemy async api type hints are out of date facepalm

--- ynot-server/app/db/db.py
+++ ynot-server/app/db/db.py
@@ -6,9 +6,9 @@ from app.config import settings
 db_url = f"postgresql+asyncpg://{settings.postgres_user}:{settings.postgres_password}@db:{settings.postgres_port}/{settings.postgres_db}"
 
 engine = create_async_engine(db_url, echo=True)
-async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)  # type: ignore
 
 
 async def get_async_session():
-    async with async_session() as session:
+    async with async_session() as session:  # type: ignore
         yield session
